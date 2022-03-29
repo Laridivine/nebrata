cdfa79bf7221e179e808d55df682118198c2a094
Kidchris <christian960@gmail.com>
2022-03-29 13:47:27 +0000

    Full body without css

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ $(document).ready(function () {
     items:6,
   });
 });
+
+
+// change the navbar backgroundcolor to black and the text color to white on page scroll
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 5) {
+    $('.navbar').addClass('black');
+    $('.nav-link').addClass('white');
+  } else {
+    $('.navbar').removeClass('black');
+    $('.nav-link').removeClass('white');
+
+  }
+});
